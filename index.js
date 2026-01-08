34621d337dc8340eadbaa5de783c2687cf3d8c86
salua <sophiemorales6440@gmail.com>
2026-01-08 12:12:10 +0100

    node.02

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-
-let number = 2;
-
-function doubleIt (value)
-{
-return value * 2;
-}
-
-doubleIt(number);
+require('dotenv').config();
+const name = process.env.MY_NAME;
+const city = process.env.MY_CITY;
+const language = process.env.MY_LANGUAGE;
+console.log(`I'm ${name}. I live in ${city} and I love programming in ${language}.`);
