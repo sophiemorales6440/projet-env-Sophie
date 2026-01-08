3c45e1b1a487fb53faf814bfb5c75383a3496cc9
salua <sophiemorales6440@gmail.com>
2026-01-08 12:02:24 +0100

    correction

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-require('dotenv').config();
-const name = process.env.MY_NAME;
-const city = process.env.MY_CITY;
-const language = process.env.MY_LANGUAGE;
-console.log(`I'm ${name}. I live in ${city} and I love programming in ${language}.`);
+
+let number = 2;
+
+function doubleIt (value)
+{
+return value * 2;
+}
+
+doubleIt(number);
